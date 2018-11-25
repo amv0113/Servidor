b37fed8f7b40d984dac9905a11e22a46a438a1a2
Angel Martinez Velasco <39508156+amv0113@users.noreply.github.com>
2018-11-24 20:24:00 -0600

    Add files via upload

---
+++ Servidor/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Servidor
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            SocketServidor svr = new SocketServidor("127.0.0.1",6969);
+            svr.Escuchar();
+        }
+    }
+}
---
+++ Servidor/SocketServidor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Servidor
+{
+    internal class SocketServidor
+    {
+        string v1;
+        int v2;
+
+        public SocketServidor(string v1, int v2)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+
+            Console.WriteLine(@"Servidor iniciado en {0}\{1}", Dns.Resolve(Dns.GetHostName()).AddressList[0].ToString(), v2);
+        }
+
+        internal void Escuchar()
+        {
+            TcpListener svr = new TcpListener(v2);
+            svr.Start();
+            while(true)
+            {
+                //ESperar que se conecten
+                Console.WriteLine("Esperando...");
+
+                TcpClient cte = svr.AcceptTcpClient();
+                Console.WriteLine("Vale verga, llego un cliente");
+
+
+                //Obtener el medio de comunicacion
+                NetworkStream stm = cte.GetStream();
+
+                //Preparamos mensaje
+                byte[] buffer = Encoding.ASCII.GetBytes("Bienvenido");
+
+                stm.Write(buffer, 0, buffer.Length);
+            }
+        }
+    }
+}
